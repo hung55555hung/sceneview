399fd8191ebda54b58d311c3c7bb1c684e83a6c6
hung55555hung <hung55555hung@gmail.com>
2026-09-08 16:07:27 +0700

    1.1

--- arsceneview/src/main/java/io/github/sceneview/ar/ARScene.kt
+++ arsceneview/src/main/java/io/github/sceneview/ar/ARScene.kt
@@ -1008,14 +1008,7 @@ fun ARSceneView(
         arCore.create(context, permissionHandler, sessionFeatures)
 
         val observer = object : DefaultLifecycleObserver {
-            override fun onResume(owner: LifecycleOwner) {
-                try {
-                    arCore.resume(context, permissionHandler)
-                }catch (e: Exception) {
-
-                }
-
-            }
+            override fun onResume(owner: LifecycleOwner) { arCore.resume(context, permissionHandler) }
             override fun onPause(owner: LifecycleOwner) { arCore.pause() }
         }
         lifecycle.addObserver(observer)
